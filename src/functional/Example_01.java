733f17ed6525c185889f9ba65cc16c183ceaca35
Shahnawaz <49604292+shahnawazm786@users.noreply.github.com>
2021-10-14 18:41:53 +0530

    Functional
    Functional

---
+++ src/functional/Example_01.java
@@ -0,0 +1,15 @@
+package functional;
+
+import java.util.*;
+public class Example_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> number=Arrays.asList(15,20,50,60,30,20,11,-9,5,6);
+		int factor=2;
+		System.out.println(number.stream().filter(num->num%2==0)
+				.mapToInt(e->e*factor).sum()
+				);
+	}
+
+}
