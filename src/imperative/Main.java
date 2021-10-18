9a22646b7d8009362fe0d96f937cff8dcd999aa2
Shahnawaz <49604292+shahnawazm786@users.noreply.github.com>
2021-10-18 12:28:06 +0530

    Imperative
    Imperative

---
+++ src/imperative/Main.java
@@ -0,0 +1,34 @@
+package imperative;
+
+import java.util.List;
+
+import imperative.Main.Person.Gender;
+
+public class Main {
+	public static void main(String[] args) {
+		List<Person> person=List.of(
+			new Person("John",Gender.MALE),
+			new Person("Maria",Gender.FEMALE),
+			new Person("Raj",Gender.MALE),
+			new Person("Sanjay",Gender.MALE)
+				);
+	}
+public static class Person{
+	private final String name;
+	private final Gender gender;
+	enum Gender{
+		MALE, FEMALE
+	}
+
+	public Person(String name,Gender gender) {
+		this.name=name;
+		this.gender=gender;
+	}
+	@Override
+	public String toString() {
+		return "Person {" + 
+				"Name = "+ name +
+				"Gender = " +gender +'}'; 
+	}
+}
+}
