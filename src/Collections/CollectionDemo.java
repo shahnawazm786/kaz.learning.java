5ecd11780e67b08b90873094cfb12494537974b8
Shahnawaz <49604292+shahnawazm786@users.noreply.github.com>
2021-10-22 12:07:45 +0530

    Collections
    Collections

---
+++ src/Collections/CollectionDemo.java
@@ -0,0 +1,22 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CollectionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Collection<String> fruits=new ArrayList<>();
+		fruits.add("banana");
+		fruits.add("apple");
+		fruits.add("mango");
+		fruits.add("pear");
+		System.out.println(fruits);
+		fruits.remove("banana");
+		System.out.println(fruits);
+		fruits.remove("orange"); //object not found but exception is not raised
+		System.out.println(fruits);
+	}
+
+}
