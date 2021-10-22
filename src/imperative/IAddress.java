eedb221fb86693278aa88b3557f717473fc50db0
Shahnawaz <49604292+shahnawazm786@users.noreply.github.com>
2021-10-22 16:34:49 +0530

    Interface Implementation
    Interface

---
+++ src/imperative/IAddress.java
@@ -0,0 +1,11 @@
+package imperative;
+
+public interface IAddress {
+String Fname="Mohammad";
+String Lname="Shahnawaz";
+String Address="New Delhi";
+String Pin="110065";
+String Phone="011-89674532";
+String Email="java-practice@kaz.com";
+
+}
