33d0ebc715f317ca2565e9c74a368c1482e49dd8
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-10 16:10:09 +0530

    cucumber bdd

---
+++ src/test/java/org/awesomecucumber/MyStepdefs.java
@@ -0,0 +1,27 @@
+package org.awesomecucumber;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @Given("I goto cart page")
+    public void iGotoCartPage() {
+        
+    }
+
+    @And("I select the item from the cart")
+    public void iSelectTheItemFromTheCart() {
+        
+    }
+
+    @When("I click on add to cart button")
+    public void iClickOnAddToCartButton() {
+        
+    }
+
+    @Then("Selected item move to the cart")
+    public void selectedItemMoveToTheCart() {
+    }
+}
